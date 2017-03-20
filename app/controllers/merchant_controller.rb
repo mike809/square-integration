b1a5317e51a340d0f9d371c1a2e8754e59edcc76
Michael Romano <michael.romano@lendup.com>
2017-03-19 22:03:33 -0700

    remove comments

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -1,19 +1,13 @@
 class MerchantController < ApplicationController
-
-  def new
-  end
-
   def callback
     authorization = SquareAuthorization.authorize!(params['code'])
 
     if authorization.has_key?(:error)
-      render plain: authorization[:error] # TODO: Error or something
+      render plain: authorization[:error]
     else
       access_token = authorization[:access_token]
       render plain: 'Success!'
-      # TODO: Store access_token securely
       Merchant.create(access_token: access_token)
-      # and use it in subsequent requests to the Connect API on behalf of the merchant.
     end
   end
 end
