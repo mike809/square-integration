88c48c4bacd2ebd82a42ed3e79ff72ef79ddde04
Michael Romano <michael.romano@lendup.com>
2017-03-21 20:18:09 -0700

    change the statement for debugging, use sandbox access token

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -5,10 +5,10 @@ class MerchantController < ApplicationController
     if authorization.has_key?(:error)
       render plain: authorization[:error]
     else
-      access_token = authorization[:access_token]
+      access_token = 'sandbox-sq0atb-MKh5RyMa3ASgbDrR16gDXw' # authorization[:access_token]
       merchant = Merchant.find_or_create_by(access_token: access_token)
       merchant.fetch_merchant_locations
-      render plain: 'Success!'
+      render plain: "Access Token: #{access_token}"
     end
   end
 end
