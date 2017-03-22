6d114dfc95987b59fffb1779328f5127a516b3f6
Michael Romano <michael.romano@lendup.com>
2017-03-22 00:47:07 -0700

    move lib to app and remove devise stuff

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,5 @@ Rails.application.routes.draw do
   post '/merchant' => 'merchant#create'
   get '/callback' => 'merchant#callback'
 
-  devise_for :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
