6762199e6ceadb486bdc1b71e736c42151a71807
Michael Romano <michael.romano@lendup.com>
2017-03-21 20:50:56 -0700

    use sandbox token for now except on test

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -5,7 +5,9 @@ class MerchantController < ApplicationController
     if authorization.has_key?(:error)
       render plain: authorization[:error]
     else
-      access_token = 'sandbox-sq0atb-MKh5RyMa3ASgbDrR16gDXw' # authorization[:access_token]
+      sandbox_access_token = Rails.application.config.sandbox_square_app_secret
+      access_token = Rails.env.test? ? authorization[:access_token] : sandbox_access_token
+
       merchant = Merchant.find_or_create_by(access_token: access_token)
       merchant.fetch_merchant_locations
       render plain: "Access Token: #{access_token}"
